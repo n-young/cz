be897552784a81f8444e3502b0e34bcabd5eb26e
Nick Young <youngnicholas228@gmail.com>
2021-05-09 09:12:38 -0700

    mobile view

--- themes/cz/layouts/partials/js/script.js
+++ themes/cz/layouts/partials/js/script.js
@@ -1,20 +1,20 @@
 // Fade in
-$(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function () {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight() / 2;
-        
-        /* If the element is completely within bounds of the window, fade it in */
-        if (!(objectBottom > windowBottom)) { //object comes into view (scrolling down)
-          $(this).addClass("faded-in")
-        } else { //object goes out of view (scrolling up)
-          $(this).removeClass("faded-in")
-        }
-      });
+$(window).on("load", function () {
+    $(window).scroll(function () {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".fade").each(function () {
+            /* Check the location of each desired element */
+            var objectBottom = $(this).offset().top + $(this).outerHeight() / 2;
+
+            /* If the element is completely within bounds of the window, fade it in */
+            if (!(objectBottom > windowBottom)) { //object comes into view (scrolling down)
+                $(this).addClass("faded-in")
+            } else { //object goes out of view (scrolling up)
+                $(this).removeClass("faded-in")
+            }
+        });
     }).scroll(); //invoke scroll-handler on page-load
-  });
+});
 
 // Lightboxes
 Array.from(document.getElementsByClassName("artimage"))
@@ -32,3 +32,12 @@ Array.from(document.getElementsByClassName("artlightbox"))
             lightbox.style.zIndex = -1
         }
     }))
+
+// Navbar
+function openNav() {
+    document.getElementById("navholder-mobile").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("navholder-mobile").style.height = "0%";
+}
