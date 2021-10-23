6e8de060ffec3159ce3892568612f2fc0733be9c
Christine Zhou <christine_zhou@brown.edu>
2021-10-23 17:04:17 -0400

    og?

--- themes/cz/layouts/partials/js/cursors.js
+++
@@ -1,7 +0,0 @@
-const cursorTag = document.querySelector("div.cursors")
-const ball = cursorTag.querySelector("div")
-
-document.addEventListener("mousemove",function(event){
-  ball.style.left = event.pageX + "px"
-  ball.style.top = event.pageY + "px"
-})
